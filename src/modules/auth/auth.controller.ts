7c4bed6c9dce69d987cde53eddba644b74c66b57
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-02-21 20:57:20 -0300

    Protect logout route with JWT guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, UseGuards, HttpCode, HttpStatus, Post, Req } from "@nestjs/common";
 import { AuthSignInUseCase } from "./use-cases/auth-login.use-case";
 import { SignInDto } from "./dto/sign-in.dto";
 import { RefreshTokenDto } from "./dto/refresh-token.dto";
+import { JwtGuard } from "./guards/jwt.guard";
+import { ApiResponseDto } from "../users/dto/api-response.dto";
+import { LogoutResponseDTO } from "./dto/logout.dto";
 
 @Controller('auth')
 export class AuthController {
@@ -19,5 +22,12 @@ export class AuthController {
     @Post('refresh')
     async refreshToken(@Body() dto: RefreshTokenDto) {
         return this.authUseCase.refreshToken(dto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(JwtGuard)
+    @Post('logout')
+    async logout(@Req() req): Promise<ApiResponseDto<LogoutResponseDTO>> {
+        return this.authUseCase.logout(req.user);
     }
 }
