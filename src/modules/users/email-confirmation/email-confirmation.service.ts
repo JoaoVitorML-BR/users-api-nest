797345f7e7ba9c2b90c0351fa2f583e195cee5e4
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-02-27 10:20:47 -0300

    feat: improve email confirmation logic and error handling

---
+++ src/modules/users/email-confirmation/email-confirmation.service.ts
@@ -0,0 +1,52 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { EmailConfirmation } from "./email-confirmation.entity";
+import { Repository } from "typeorm";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { User } from "../user.entity";
+
+@Injectable()
+export class EmailConfirmationService {
+    constructor(
+        @InjectRepository(EmailConfirmation) private readonly emailConfirmationEntity: Repository<EmailConfirmation>,
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) { }
+
+    async saveToken(email: string, token: string): Promise<EmailConfirmation> {
+        try {
+            const user = await this.userRepository.findOne({ where: { email } });
+            if (!user) {
+                throw new Error('User not found for the provided email.');
+            }
+
+            const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000); // Token expires in 24 hours
+            const emailConfirmation = this.emailConfirmationEntity.create({ token, user, expiresAt });
+            return await this.emailConfirmationEntity.save(emailConfirmation);
+        } catch (error) {
+            throw new Error(`Failed to save token: ${error.message}`);
+        }
+    }
+
+    async activateAccount(token: string) {
+        const emailConfirmation = await this.emailConfirmationEntity.findOne({
+            where: { token },
+            relations: ['user'],
+        });
+
+        if (!emailConfirmation) {
+            throw new BadRequestException('Token not found.');
+        }
+        if (!emailConfirmation.expiresAt) {
+            throw new BadRequestException('Token expiration date not set.');
+        }
+        if (emailConfirmation.expiresAt < new Date()) {
+            await this.emailConfirmationEntity.remove(emailConfirmation);
+            throw new BadRequestException('Token has expired. Please request a new one.');
+        }
+
+        emailConfirmation.user.isActive = true;
+        emailConfirmation.user.emailConfirmed = true;
+        await this.emailConfirmationEntity.remove(emailConfirmation);
+        await this.userRepository.save(emailConfirmation.user);
+        return { statusCode: 200, status: true, message: 'Account activated successfully!' };
+    }
+}
