901469fefa7fa53eeb94f76fc9bb2290cc866271
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-02-27 10:21:12 -0300

    fix: adjust account activation endpoint to receive token from body and improve error handling

---
+++ src/modules/users/email-confirmation/email-confirmation.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Patch, Post } from "@nestjs/common";
+import { SendTokenUseCase } from "./use-cases/send-token.use-case";
+import { InjectQueue } from "@nestjs/bull";
+import type { Queue } from 'bull';
+import { SendEmailDto } from "./dto/send-email.dto";
+import { ActivateAccountUseCase } from "./use-cases/activate-account.use-case";
+
+@Controller('email-confirmation')
+export class EmailConfirmationTokenController {
+    constructor(
+        @InjectQueue('email') private readonly emailQueue: Queue,
+        private readonly activateAccountUseCase: ActivateAccountUseCase,
+        private readonly sendTokenUseCase: SendTokenUseCase
+    ) { }
+
+    @Patch('activate-account')
+    async ActivateAccount(@Body() body: { token: string }) {
+        const emailConfirmation = await this.activateAccountUseCase.execute(body.token);
+        return emailConfirmation;
+    }
+
+    @Post('teste-send-email')
+    async testeSendEmail() {
+        const sendToken = await this.sendTokenUseCase.execute({ email: "jvmlira33@gmail.com" });
+        return sendToken;
+    }
+
+    @Post('send-email')
+    async sendEmail(@Body() dto: SendEmailDto) {
+        await this.emailQueue.add({
+            email: dto.email,
+            token: dto.token,
+        });
+        return { message: 'E-mail será enviado em background.' };
+    }
+}
+
+// fiz o cadastro > gero o token > salvo o token no banco se o email nãoe xistir ainda > envio o email com o token > o usuário clica no link do email > pego o token do link > valido o token > se for válido, ativo a conta do usuário e deleto o token do banco
+// para isso no controller > recebo o token > chamo o use case > no use case chamo o service para validar o token > se for válido, ativo a conta do usuário e deleto o token do banco
