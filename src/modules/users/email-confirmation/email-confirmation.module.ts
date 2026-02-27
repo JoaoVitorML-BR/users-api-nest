4a8628acdea10d7384221549653754531d07350a
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-02-27 10:24:12 -0300

    fix: update email confirmation module providers and imports

---
+++ src/modules/users/email-confirmation/email-confirmation.module.ts
@@ -0,0 +1,30 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { EmailConfirmation } from "./email-confirmation.entity";
+import { EmailConfirmationTokenController } from "./email-confirmation.controller";
+import { SendTokenUseCase } from "./use-cases/send-token.use-case";
+import { EmailConfirmationService } from "./email-confirmation.service";
+import { Module } from "@nestjs/common";
+import { TokenGeneratorService } from "src/common/services/token-generator.service";
+import { SendTokenFromEmailService } from "src/common/services/send-email-token.service";
+import { BullModule } from "@nestjs/bull";
+import { User } from "../user.entity";
+import { ActivateAccountUseCase } from "./use-cases/activate-account.use-case";
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([EmailConfirmation, User]),
+        BullModule.registerQueue({
+            name: 'email',
+        }),
+    ],
+    controllers: [EmailConfirmationTokenController],
+    providers: [
+        EmailConfirmationService,
+        SendTokenUseCase,
+        TokenGeneratorService,
+        SendTokenFromEmailService,
+        ActivateAccountUseCase,
+    ],
+    exports: [EmailConfirmationService, SendTokenUseCase],
+})
+export class EmailConfirmationModule { }
