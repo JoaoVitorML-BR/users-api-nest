15f58b66bca363b6edfef048a8669418b4f078c4
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-03-03 19:26:13 -0300

    feat: strengthen RefreshTokenDto validation

--- src/modules/auth/dto/refresh-token.dto.ts
+++ src/modules/auth/dto/refresh-token.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class RefreshTokenDto {
     @IsNotEmpty()
     @IsString()
+    @MinLength(20)
     refreshToken: string;
 }
