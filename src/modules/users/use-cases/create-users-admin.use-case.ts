4826623a4f69ce83a86225df0d220f9b7ca14be4
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-03-04 12:01:59 -0300

    feat: add sendToken to email to activate admin

--- src/modules/users/use-cases/create-users-admin.use-case.ts
+++ src/modules/users/use-cases/create-users-admin.use-case.ts
@@ -6,11 +6,13 @@ import { ROLE } from "../user.entity";
 import * as bcrypt from 'bcrypt';
 import { UserService } from "../user.service";
 import { ResponseCreateUsersDto } from "../dto/response-create-users.dto";
+import { SendTokenUseCase } from "../email-confirmation/use-cases/send-token.use-case";
 
 @Injectable()
 export class CreateUsersAdminUseCase {
     constructor(
-        private readonly userService: UserService
+        private readonly userService: UserService,
+        private readonly sendTokenUseCase: SendTokenUseCase
     ) { }
 
     async createAdmin(Data: CreateUserAdminDTO): Promise<ApiResponseDto<ResponseCreateUsersDto>> {
@@ -19,18 +21,19 @@ export class CreateUsersAdminUseCase {
                 throw new BadRequestException("Name, username, email and password are required");
             }
 
+            const hashedPassword = await bcrypt.hash(Data.password, 10);
+
             const userExists = await this.userService.checkUserExistsByEmailAndUsername(Data.email, Data.username);
             if (userExists) {
                 throw new ConflictException('User with the same email or username already exists');
             }
-
-            const hashedPassword = await bcrypt.hash(Data.password, 10);
 
             const res = await this.userService.create({ ...Data, password: hashedPassword, role: ROLE.ADMIN });
             if (!res || !res.id) {
                 throw new InternalServerErrorException('Failed to create user');
             }
 
+            this.sendTokenUseCase.execute({ email: res.email });
             return {
                 statusCode: 201,
                 code: "SUCCESS",
