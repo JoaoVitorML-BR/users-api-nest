1d2b58ac009647fca98243858749076c9ca93e9b
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-03-12 12:38:11 -0300

    Return paginated users

--- src/modules/users/use-cases/find-all-users.use-case.ts
+++ src/modules/users/use-cases/find-all-users.use-case.ts
@@ -1,18 +1,26 @@
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { UserService } from "../user.service";
 import { FindAllUsersResponseDto } from "../dto/find-all-uses-response.dto";
+import { PageDto } from "../dto/page.dto";
+import { PageMetaDto } from "../dto/page-meta.dto";
+import { PageOptionsDto } from "../dto/page-options.dto";
 
 @Injectable()
 export class FindAllUsersUseCase {
     constructor(private readonly userService: UserService) { }
 
-    async findAll(): Promise<FindAllUsersResponseDto[]> {
-        const res = await this.userService.findAll();
+    async findAll(pageOptionsDto: PageOptionsDto): Promise<PageDto<FindAllUsersResponseDto>> {
+        const [users, itemCount] = await this.userService.findAll(pageOptionsDto);
 
-        if (!res) {
+        if (!users) {
             throw new InternalServerErrorException('Failed to retrieve users');
         }
 
-        return res;
+        const meta = new PageMetaDto({
+            pageOptionsDto,
+            itemCount,
+        });
+
+        return new PageDto(users, meta);
     }
 }
